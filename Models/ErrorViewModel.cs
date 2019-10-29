b149afd8f5165c9bb1180702ec96bdccc3dc9559
anloffredo <anloffredo@massageenvy.com>
2019-10-29 13:49:17 -0700

    initial check in

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Penguin.Cms.Template.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Penguin.Cms.Web.Mvc;
+using System;
+
+namespace Penguin.Cms.Client
+{
+    public static class Program
+    {
+        public static DateTime BootTime { get; set; } = DateTime.Now;
+
+        public static void Main()
+        {
+
+            Penguin.Cms.Web.Mvc.HostBuilder.Configure().ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseIIS()
+                .ConfigureKestrel(serverOptions =>
+                {
+                    // Set properties and call methods on options
+                })
+
+                .UseIISIntegration()
+                .UseStartup<Startup>();
+            })
+            .Build().Run();
+
+        }
+    }
+}

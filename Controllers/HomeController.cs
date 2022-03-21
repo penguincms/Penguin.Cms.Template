49728c0bf1c5e4e562f0f340590dafa1c74b90e4
MrJackSpade <loffredo.ap@gmail.com>
2022-03-20 21:36:00 -0700

    Autocommit
    Autocommit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,19 +7,10 @@ namespace Penguin.Cms.Template.Controllers
     public class HomeController : Controller
     {
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return this.View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
-        }
+        public IActionResult Error() => this.View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
 
-        public IActionResult Index()
-        {
-            return this.View();
-        }
+        public IActionResult Index() => this.View();
 
-        public IActionResult Privacy()
-        {
-            return this.View();
-        }
+        public IActionResult Privacy() => this.View();
     }
 }

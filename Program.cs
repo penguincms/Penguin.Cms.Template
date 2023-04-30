2d0098e72992fdd9519ab48b0cc966823b9d7ef1
Anthony Loffredo <loffredo.ap@gmail.com>
2023-04-30 11:41:43 -0700

    Autocommit
    Autocommit

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using Penguin.Cms.Web.Mvc;
 using System;
 
 namespace Penguin.Cms.Template
---
+++ Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Penguin.Authentication.Abstractions.Interfaces;
+using Penguin.Authentication.Domain;
+
+namespace Penguin.Cms.Template
+{
+	public class Startup : Penguin.Cms.Web.Mvc.Startup
+	{
+		public Startup(IConfiguration configuration) : base(configuration)
+		{
+		}
+
+		public override void ConfigureAdditionalServices(IServiceCollection services)
+		{
+			services.AddSingleton<IAuthenticator, DomainAuthenticator>();
+		}
+	}
+}
